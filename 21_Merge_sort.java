238802063c02c9c2f01957860e02972ad96298bf
G V Sai Rakesh <71537500+Rakesh-03@users.noreply.github.com>
2023-06-14 20:57:51 +0530

    Update 21_Merge_sort.java

--- 21_Merge_sort.java
+++ 21_Merge_sort.java
@@ -1,3 +1,5 @@
+# NOT INPLACE SORTING
+
 import java.util.Arrays;
 import java.util.*;
 public class merge_sort 
@@ -76,3 +78,90 @@ public class merge_sort
 	}
 
 }
+
+
+
+# INPLACE SORTING - MERGE SORT
+
+import java.util.Arrays;
+import java.util.*;
+public class merge_sort 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array : ");
+		int n = sc.nextInt();
+		
+		System.out.println("Enter the elements of the array : ");
+		int arr[] = new int[n];
+		for(int i = 0 ; i < n ;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		mergesort_Inplace(arr,0,arr.length);
+		 
+		 System.out.println(Arrays.toString(arr));
+
+	}
+	
+	public static void mergesort_Inplace(int arr[],int s, int e)
+	{
+	    if(e-s == 1)
+	    {
+	    	return ;
+	    }
+	    
+	    int mid = (s+e)/2;
+	    
+	    mergesort_Inplace(arr, s, mid);
+	    mergesort_Inplace(arr, mid, e);
+	    
+	    
+		merge(arr,s,mid,e);
+	}
+	
+	public static void merge(int[] arr ,int s,int mid , int e)
+	{
+		
+		int i = s, j = mid , k=0;
+		int[] arr_1 = new int[e-s];
+		
+		while(i < mid && j < e)
+		{
+ 			if(arr[i] < arr[j])
+			{
+				arr_1[k] = arr[i];
+				i++;
+			}
+			else
+			{
+				arr_1[k] = arr[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i < mid)
+		{
+			arr_1[k] = arr[i];
+			i++;
+			k++;
+		}
+		
+		while(j < e)
+		{
+			arr_1[k] = arr[j];
+			j++;
+			k++;
+		}
+		
+		for(int a = 0 ; a < arr_1.length;a++)
+		{
+			arr[s+a]  = arr_1[a];
+		}
+	}
+
+}
+
